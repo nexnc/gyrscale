bdd276da6502319bd2e8be3a2c8fd4ae47ed2c51
nexnc <git@nexnc.com>
2026-05-05 13:15:50 +0200

    feat: added basic functionality of printing OS Name, CPU Brand and Memory Usage

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use sysinfo::System;
+
 fn main() {
-    println!("Hello, world!");
+    let mut sys = System::new_all();
+    sys.refresh_all();
+    
+    if let Some(os_name) = System::name() {
+        println!("OS Name: {}", os_name);
+    }
+
+    // Get CPU Name
+    if let Some(cpu) = sys.cpus().first() {
+        println!("CPU Name {}", cpu.brand());
+    }
+
+    let used_ram = sys.used_memory() / 1024 / 1024 / 1024;
+    let total_ram = sys.total_memory() / 1024 / 1024 / 1024;
+
+    println!("RAM:    {} GB / {} GB", used_ram, total_ram);
+
+
 }
