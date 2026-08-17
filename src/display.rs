90f581363459615c5c5bc494b54e2af51a912b81
nexnc <git@nexnc.com>
2026-08-17 10:44:50 +0200

    feat: added support for cpu, architecture, shell, terminal, core and thread count, swap, de/wm and user

--- src/display.rs
+++ src/display.rs
@@ -4,13 +4,16 @@ pub fn print_fetch(data: &SystemData) {
     let separator = "-------------------------";
 
     println!();
-    println!("[S Y S T E M   I N F O]");
+    println!("{0}@{1}", data.user,data.host_name);
     println!("{separator}");
-    println!("OS:     {0}", data.os_name);
-    println!("OS Version: {0}", data.os_version);
-    println!("Hostname: {0}", data.host_name);
+    println!("OS: {0} {1} {2}",data.os_name, data.os_version, data.cpu_arch);
     println!("Kernel: {0}", data.kernel_version);
     println!("Uptime: {0}", data.format_uptime());
+    println!("Shell: {0}", data.shell);
+    println!("Terminal: {0}", data.terminal);
+    println!("WM: {0}", data.wm);
+    println!("CPU: {0} ({1}C/{2}T)",data.cpu, data.core_count, data.thread_count);
     println!("Memory: {0}", data.format_memory());
+    println!("Swap: {0}", data.format_swap());
     println!("{separator}\n");
 }
